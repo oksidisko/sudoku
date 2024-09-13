5712da0a46266ffdb1d21fa7e186f25a7cff0555
Irina Vikhrova <ivikhrova@optimalgroup.ru>
2024-09-13 10:46:54 +0300

    #1: set vite bundle + add initial screen

---
+++ sudoku-front/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className={'start'}>
+        <h1>Sudoku</h1>
+        <button>Начать игру</button>
+      </div>
+    </>
+  )
+}
+
+export default App
